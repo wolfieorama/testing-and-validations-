6e586735c9e8e3c89b236ad5b649f4f3b823aedd
wolfieorama <wachirawamunyi@gmail.com>
2015-11-03 13:07:30 +0300

    added dashboards with sample random data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount Dashing::Engine, at: Dashing.config.engine_path
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
