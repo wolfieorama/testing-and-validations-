d7e6cdef9d84640a63b21f198cb5060a5c49940d
wolfieorama <wachirawamunyi@gmail.com>
2015-11-04 13:17:12 +0300

    testing for image url and title replication

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
+
   test "product attributes must not be empty" do
     product = Product.new
     assert product.invalid?
@@ -26,6 +28,34 @@ class ProductTest < ActiveSupport::TestCase
 
     product.price = 1
     assert product.valid?
+  end
 
+  def new_product(image_url)
+    Product.new(title: "my book is here",
+                description: "yes it is here holla",
+                price: 1,
+                image_url: image_url)
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+    ok.each do |name|
+      assert new_product(name).valid?, "#{name} should be valid"
+    end
+    bad.each do |name|
+      assert new_product(name).invalid?, "#{name} should n't be valid"
+    end
+  end
+
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+                          description: "yyy",
+                          price: 1,
+                          image_url: "fred.gif")
+    assert product.invalid?
+    # Using built in error message table
+    # assert_equal [118n.translate('errors.messages.taken')], product.errors[:title]
+    assert_equal ["has already been taken"], product.errors[:title]
   end
 end
