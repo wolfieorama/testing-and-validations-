268e49fb2591439bbc8c3d6df7feb774d9c38fcc
wolfieorama <wachirawamunyi@gmail.com>
2015-11-04 11:09:08 +0300

    added test modules for price validation

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -9,4 +9,23 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:price].any?
     assert product.errors[:image_url].any?
   end
+
+  test "product price must be positive" do
+    product = Product.new(title: "my book title",
+                          description: "this is just a book",
+                          image_url: "image.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ["must be greater than or equal to 0.01"],
+      product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+
+  end
 end
